dce76a89173573929b778e39b7a7ce5d0f88b983
nvdfxx <artem7565@gmail.com>
2018-10-02 15:27:44 +0400

    add post requests

---
+++ src/addpost.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="addpost">
+        <div class="posttitle">
+            <input type="text" v-model="postTitle" placeholder="Post title">
+        </div>
+        <div class="postbody">
+            <textarea v-model="postBody" placeholder="Post body"></textarea>
+        </div>
+        <button @click="addPost()">
+            Post request
+        </button>
+        <div class="resp">
+            {{response.data}}
+        </div>
+    </div>
+</template>
+
+<script>
+
+	import axios from 'axios'
+	
+	export default {
+		data() {
+			return {
+				postTitle: '',
+				postBody: '',
+				response: []
+			}
+		},
+
+		methods: {
+			addPost() {
+	            axios.post('https://jsonplaceholder.typicode.com/posts', {title: this.postTitle, body: this.postBody})
+	            .then(response => (this.response = response))
+	            .catch(error => console.log(error))
+	        }
+		}
+	}
+
+</script>
+
+<style>
+	.postbody {
+		margin-top: 10px;
+	}
+</style>
